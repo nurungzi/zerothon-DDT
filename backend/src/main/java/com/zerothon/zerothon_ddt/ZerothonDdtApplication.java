1e4374c7aace5b5d85c68f3c7376c778adb29930
nurungzi <jj020301@gmail.com>
2025-03-27 01:05:44 +0900

    dev: database connection test code
    일단 올려봐

--- backend/src/main/java/com/zerothon/zerothon_ddt/ZerothonDdtApplication.java
+++ backend/src/main/java/com/zerothon/zerothon_ddt/ZerothonDdtApplication.java
@@ -2,8 +2,10 @@ package com.zerothon.zerothon_ddt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "common.entity")
 public class ZerothonDdtApplication {
 
 	public static void main(String[] args) {
---
+++ backend/src/main/java/common/repository/CommonRepository.java
@@ -0,0 +1,8 @@
+package common.repository;
+
+import common.entity.Common;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommonRepository extends JpaRepository<Common, Integer> {
+
+}
