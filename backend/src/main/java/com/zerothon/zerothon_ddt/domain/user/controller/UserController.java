78a528ea5bd5bff893a1a8c47e7e5f2eb17e2679
nurungzi <jj020301@gmail.com>
2025-03-27 23:15:56 +0900

    dev: add login api

--- backend/src/main/java/com/zerothon/zerothon_ddt/domain/user/controller/UserController.java
+++ backend/src/main/java/com/zerothon/zerothon_ddt/domain/user/controller/UserController.java
@@ -3,10 +3,7 @@ package com.zerothon.zerothon_ddt.domain.user.controller;
 import com.zerothon.zerothon_ddt.domain.user.dto.UserDTO;
 import com.zerothon.zerothon_ddt.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -17,5 +14,10 @@ public class UserController {
     @PostMapping()
     public UserDTO.UserResponse createUser(@RequestBody UserDTO.UserRequest request){
         return userService.createUser(request);
+    }
+
+    @GetMapping("/login")
+    public UserDTO.UserResponse login(@RequestParam String loginId, @RequestParam String password){
+        return userService.loginUser(loginId, password);
     }
 }
--- backend/src/main/java/com/zerothon/zerothon_ddt/domain/user/service/UserService.java
+++ backend/src/main/java/com/zerothon/zerothon_ddt/domain/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.zerothon.zerothon_ddt.domain.user.service;
 
+import com.zerothon.zerothon_ddt.common.error.Message;
+import com.zerothon.zerothon_ddt.common.exception.GlobalException;
 import com.zerothon.zerothon_ddt.domain.user.dto.UserDTO;
 import com.zerothon.zerothon_ddt.domain.user.entity.User;
 import com.zerothon.zerothon_ddt.domain.user.repository.UserQueryRepository;
@@ -18,6 +20,12 @@ public class UserService {
     public UserDTO.UserResponse createUser(UserDTO.UserRequest request){
         User user = userRepository.save(request.toEntity());
 
+        return new UserDTO.UserResponse(user);
+    }
+
+    @Transactional
+    public UserDTO.UserResponse loginUser(String loginId, String password){
+        User user = userQueryRepository.findLogin(loginId, password).orElseThrow(() -> new GlobalException(Message.CANNOT_LOGIN.getMessage()));
         return new UserDTO.UserResponse(user);
     }
 }
